c337a5e29f622ab0c7e05a48b307dd3b20ae40a6
Judahh <judahholanda7@gmail.com>
2017-10-05 09:09:37 -0300

    update

--- event/operation.ts
+++
@@ -1,8 +0,0 @@
-export enum Operation {
-    add,
-    read,
-    correct,
-    update,
-    nonexistent,
-    delete
-}
--- index.ts
+++
@@ -1,7 +0,0 @@
-import { Handler } from "./handler/handler";
-import { Event } from "./event/event";
-import { Operation } from "./event/operation";
-import { Write } from "./write/write";
-import { Read } from "./read/read";
-import { PersistenceAdapter } from "./persistenceAdapter/persistenceAdapter";
-export { Handler, Event, Operation, Write, Read, PersistenceAdapter }
