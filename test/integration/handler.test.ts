2e39ef4b637dcd0baa0ebd6365080e5e08400ba2
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2020-11-19 11:07:32 -0300

    add more tests and fix updateArray

--- test/integration/handler.test.ts
+++ test/integration/handler.test.ts
@@ -108,12 +108,242 @@ test('add and read array and find object', async (done) => {
       })
     );
   } catch (error) {
+    await handler.addEvent(
+      new Event({ operation: Operation.delete, name: 'object', single: false })
+    );
+    await handler.getWrite().clear('events');
     console.error(error);
     await read.close();
     await write.close();
     expect(error).toBe(null);
     done();
   }
+  await handler.addEvent(
+    new Event({ operation: Operation.delete, name: 'object', single: false })
+  );
+  await handler.getWrite().clear('events');
+  await read.close();
+  await write.close();
+  done();
+});
+
+test('add an array and read array and find object', async (done) => {
+  const journaly = Journaly.newJournaly() as SubjectObserver<any>;
+  read = new MongoDB(
+    new PersistenceInfo(
+      {
+        database: 'read',
+        host: process.env.MONGO_HOST || 'localhost',
+        port: process.env.MONGO_PORT,
+      },
+      journaly
+    )
+  );
+  write = new MongoDB(
+    new PersistenceInfo(
+      {
+        database: 'write',
+        host: process.env.MONGO_HOST || 'localhost',
+        port: process.env.MONGO_PORT,
+      },
+      journaly
+    )
+  );
+  const handler = new Handler(write, read);
+  await handler.getWrite().clear('events');
+  const obj = new Object();
+  obj['test'] = 'test';
+  const obj2 = new Object();
+  obj2['test'] = 'test2';
+
+  const objArray = [obj, obj2];
+  try {
+    await handler.addEvent(
+      new Event({ operation: Operation.delete, name: 'object' })
+    );
+
+    const persistencePromise = await handler.addEvent(
+      new Event({
+        operation: Operation.create,
+        name: 'object',
+        content: objArray,
+      })
+    );
+
+    expect(persistencePromise).toStrictEqual(
+      new PersistencePromise({
+        receivedItem: [
+          {
+            __v: persistencePromise.receivedItem[0].__v,
+            _id: persistencePromise.receivedItem[0]._id,
+            test: 'test',
+          },
+          {
+            __v: persistencePromise.receivedItem[1].__v,
+            _id: persistencePromise.receivedItem[1]._id,
+            test: 'test2',
+          },
+        ],
+        result: [undefined, undefined],
+        selectedItem: undefined,
+        sentItem: objArray,
+      })
+    );
+
+    const persistencePromise0 = await handler.addEvent(
+      new Event({
+        operation: Operation.update,
+        name: 'object',
+        single: false,
+        selection: {},
+        content: { test: 'object' },
+      })
+    );
+
+    expect(persistencePromise0).toStrictEqual(
+      new PersistencePromise({
+        receivedItem: {
+          n: 2,
+          nModified: 2,
+          ok: 1,
+        },
+        result: undefined,
+        selectedItem: {},
+        sentItem: { test: 'object' },
+      })
+    );
+
+    const persistencePromise1 = await handler.readArray('object', {});
+
+    expect(persistencePromise1).toStrictEqual(
+      new PersistencePromise({
+        receivedItem: [
+          {
+            __v: persistencePromise1.receivedItem[0].__v,
+            _id: persistencePromise1.receivedItem[0]._id,
+            test: 'object',
+          },
+          {
+            __v: persistencePromise1.receivedItem[1].__v,
+            _id: persistencePromise1.receivedItem[1]._id,
+            test: 'object',
+          },
+        ],
+        result: undefined,
+        selectedItem: {},
+        sentItem: undefined,
+      })
+    );
+
+    const persistencePromise2 = await handler.addEvent(
+      new Event({ operation: Operation.delete, name: 'object', single: false })
+    );
+
+    // console.log(persistencePromise2);
+
+    expect(persistencePromise2).toStrictEqual(
+      new PersistencePromise({
+        receivedItem: undefined,
+        result: undefined,
+        selectedItem: undefined,
+        sentItem: undefined,
+      })
+    );
+
+    const persistencePromise3 = await handler.addEvent(
+      new Event({
+        operation: Operation.create,
+        name: 'object',
+        content: obj,
+      })
+    );
+
+    expect(persistencePromise3).toStrictEqual(
+      new PersistencePromise({
+        receivedItem: {
+          __v: persistencePromise3.receivedItem.__v,
+          _id: persistencePromise3.receivedItem._id,
+          test: 'test',
+        },
+        result: undefined,
+        selectedItem: undefined,
+        sentItem: obj,
+      })
+    );
+
+    const persistencePromise31 = await handler.addEvent(
+      new Event({
+        operation: Operation.update,
+        name: 'object',
+        single: true,
+        selection: {},
+        content: { test: 'object' },
+      })
+    );
+
+    expect(persistencePromise31).toStrictEqual(
+      new PersistencePromise({
+        receivedItem: {
+          __v: persistencePromise3.receivedItem.__v,
+          _id: persistencePromise3.receivedItem._id,
+          test: 'test',
+        },
+        result: undefined,
+        selectedItem: {},
+        sentItem: { test: 'object' },
+      })
+    );
+
+    const persistencePromise4 = await handler.addEvent(
+      new Event({
+        operation: Operation.delete,
+        name: 'object',
+        selection: { id: persistencePromise3.receivedItem._id },
+      })
+    );
+    // console.log('persistencePromise3:', persistencePromise3);
+    // console.log('persistencePromise4:', persistencePromise4);
+
+    expect(JSON.stringify(persistencePromise4)).toStrictEqual(
+      JSON.stringify(
+        new PersistencePromise({
+          receivedItem: {
+            _id: persistencePromise3.receivedItem._id,
+            test: 'object',
+            __v: persistencePromise3.receivedItem.__v,
+          },
+          result: undefined,
+          selectedItem: persistencePromise3.receivedItem._id,
+          sentItem: undefined,
+        })
+      )
+    );
+
+    const persistencePromise5 = await handler.readArray('object', {});
+
+    expect(persistencePromise5).toStrictEqual(
+      new PersistencePromise({
+        receivedItem: [],
+        result: undefined,
+        selectedItem: {},
+        sentItem: undefined,
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    await handler.addEvent(
+      new Event({ operation: Operation.delete, name: 'object', single: false })
+    );
+    await handler.getWrite().clear('events');
+    await read.close();
+    await write.close();
+    expect(error).toBe(null);
+    done();
+  }
+  await handler.addEvent(
+    new Event({ operation: Operation.delete, name: 'object', single: false })
+  );
+  await handler.getWrite().clear('events');
   await read.close();
   await write.close();
   done();
