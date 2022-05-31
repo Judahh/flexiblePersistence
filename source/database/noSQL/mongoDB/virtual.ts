12bb75c235046d2a0e18164f60e7d8f8fe8e8d08
Judah <judahholanda7@gmail.com>
2022-05-31 20:50:38 -0300

    improve virtual

--- source/database/noSQL/mongoDB/virtual.ts
+++ source/database/noSQL/mongoDB/virtual.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
 export type Virtual = {
-  // eslint-disable-next-line no-unused-vars
-  [key: string]: { get?: () => unknown; set?: (value: unknown) => void };
+  [key: string]: {
+    get?: (value?: any, virtual?: any, doc?: any) => any;
+    set?: (value?: any, virtual?: any, doc?: any) => any;
+  };
 };
