2bc1ac4b4ddfc2e312d0c86746d8c6b336a010d2
Judah <judahholanda7@gmail.com>
2023-03-04 11:15:07 -0300

    Fix mongoose version

--- source/database/noSQL/mongoDB/anyModel.ts
+++ source/database/noSQL/mongoDB/anyModel.ts
@@ -1,4 +1,12 @@
-import { Model, ResolveSchemaOptions, Schema } from 'mongoose';
+import {
+  Model,
+  ResolveSchemaOptions,
+  Schema,
+  // Document,
+  // HydratedDocument,
+  // InferSchemaType,
+  // ObtainSchemaGeneric,
+} from 'mongoose';
 type AnyModel = Model<
   { [x: string]: any },
   Record<string, unknown>,
@@ -15,5 +23,21 @@ type AnyModel = Model<
     { [x: string]: any }
   >
 >;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// type AnyModel<TSchema = any> = Model<
+//   InferSchemaType<TSchema>,
+//   ObtainSchemaGeneric<TSchema, 'TQueryHelpers'>,
+//   ObtainSchemaGeneric<TSchema, 'TInstanceMethods'>,
+//   ObtainSchemaGeneric<TSchema, 'TVirtuals'>,
+//   HydratedDocument<
+//     InferSchemaType<TSchema>,
+//     ObtainSchemaGeneric<TSchema, 'TVirtuals'> &
+//       ObtainSchemaGeneric<TSchema, 'TInstanceMethods'>,
+//     ObtainSchemaGeneric<TSchema, 'TQueryHelpers'>
+//   >,
+//   TSchema
+// > &
+//   ObtainSchemaGeneric<TSchema, 'TStaticMethods'>;
 
 export type { AnyModel };
